ec3afce1f32d9fdc9dd82d9f14690b7ab904fc38
l4ncelot5 <filipmohaupt@gmail.com>
2025-04-15 09:19:59 +0200

    Pridani Class1
    pro pocitani kolikrat uzivatel pouzil kalkulacku

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class IndexerUziti
+{
+	public int pocetUziti = 0;
+
+	public IndexerUziti()
+	{
+	}
+
+	public void PridejUziti() 
+	{
+		pocetUziti++;
+	}
+	public void KolikUziti() 
+	{
+		Console.WriteLine($"Program kalkulačka jste použili {pocetUziti}-krát.");
+	}
+
+	public void ResetUziti() 
+	{ 
+		pocetUziti = 0; 
+	}
+}
